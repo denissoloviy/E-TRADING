c0081d9cc989e9fc078ba7ba60f842d184817ddf
Yurii Hanzha <yurij.ganzha@gmail.com>
2017-04-10 01:57:18 +0300

    added moc for buying

---
+++ E-TRADING/Models/ProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_TRADING.Models
+{
+    public class ProductViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+        public string Status { get; set; }
+
+        public string SellerId { get; set; }
+        public string SellerName { get; set; }
+    }
+}
