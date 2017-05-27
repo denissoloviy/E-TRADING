b94d8e60437257ed9fc6c14416ef3ede63811974
Yurii Hanzha <yurij.ganzha@gmail.com>
2017-05-27 16:56:52 +0300

    added image upload

--- E-TRADING/Controllers/ProductController.cs
+++ E-TRADING/Controllers/ProductController.cs
@@ -17,14 +17,17 @@ namespace E_TRADING.Controllers
         IMapper _mapper;
         IProductRepository _productRepository;
         ICategoryRepository _categoryRepository;
+        IImageRepository _imageRepository;
 
         public ProductController(IMapper mapper,
             IProductRepository productRepository,
-            ICategoryRepository categoryRepository)
+            ICategoryRepository categoryRepository,
+            IImageRepository imageRepository)
         {
             _mapper = mapper;
             _productRepository = productRepository;
             _categoryRepository = categoryRepository;
+            _imageRepository = imageRepository;
         }
         // GET: Product
         public ActionResult Index(string id)
@@ -80,7 +83,7 @@ namespace E_TRADING.Controllers
             _productRepository.Add(product);
             _productRepository.SaveChanges();
 
-            return RedirectToAction("UploadImages");
+            return RedirectToAction("UploadImages", new { id = product.Id });
         }
         
         public ActionResult Delete(string id)
@@ -90,37 +93,82 @@ namespace E_TRADING.Controllers
             return RedirectToAction("Products", "Seller");
         }
 
-        //[HttpPost]
-        //[Authorize(Roles =UserRole.Seller)]
-        //public ActionResult UploadImage(string id, HttpPostedFileBase image)
-        //{
-        //    var imageId = Guid.NewGuid();
-        //    var path = @"~/Content/Images/" + imageId;
+        [HttpGet]
+        [Authorize(Roles = UserRole.Seller)]
+        public ActionResult UploadImages(string id)
+        {
+            var product = _productRepository.FirstOrDefault(p => p.Id == id);
 
-        //    if (image == null)
-        //        ModelState.AddModelError("ImageFile", "Виберіть зображення");
+            if (product.SellerId != User.Identity.GetUserId())
+                return View("Error");
 
-        //    if (!ValidateFileExtension())
-        //    {
-        //        ModelState.AddModelError("ImageFile",
-        //            $"Виберіть файл відповідного формату: .bmp, .png, .jpg, .jpeg");
-        //    }
-        //    else
-        //    {
-        //        if (!string.IsNullOrEmpty(model.Image))
-        //        {
-        //            if (_fileManager.FileExists(Path.Combine(path, model.Image), true))
-        //            {
-        //                _fileManager.DeleteFile(Path.Combine(path, model.Image), true);
-        //            }
-        //        }
-        //        var fileName = news.Slug + Path.GetExtension(model.ImageFile.FileName);
-        //        _fileManager.UploadFile(model.ImageFile, path, fileName, true);
-        //        news.Image = fileName;
-        //    }
+            ViewBag.ProductId = id;
+            ViewBag.ProductName = product.Name;
+            return View(product.Images.OrderBy(i => i.Index));
+        }
 
-        //    return View();
-        //}
+        [HttpPost]
+        [Authorize(Roles = UserRole.Seller)]
+        public ActionResult UploadImages(string id, HttpPostedFileBase image)
+        {
+            var imageId = Guid.NewGuid();
+            var path = @"~/Content/ProductImages";
+
+            if (image == null)
+                return RedirectToAction("UploadImages", new { id = id });
+
+            var ext = Path.GetExtension(image.FileName);
+
+            if (!new string[] { ".jpg", ".jpeg", ".gif", ".bmp", ".png" }.Contains(Path.GetExtension(ext)))
+                return RedirectToAction("UploadImages", new { id = id });
+
+            var product = _productRepository.FirstOrDefault(p => p.Id == id);
+
+            var img = new Image()
+            {
+                Index = product.Images.Count > 0? product.Images.Select(i => i.Index).Max() + 1: 1,
+                Extention = ext
+            };
+
+            //saving image to local folder
+            image.SaveAs(Path.Combine(Server.MapPath(path), img.Id + img.Extention));
+
+            //saving image link to db
+            product.Images.Add(img);
+            _productRepository.SaveChanges();
+
+            ViewBag.ProductId = id;
+            ViewBag.ProductName = product.Name;
+            return View(product.Images.OrderBy(i => i.Index));
+        }
+
+        [HttpPost]
+        public ActionResult EditImage(string id, string imageId, int? index)
+        {
+            var product = _productRepository.FirstOrDefault(p => p.Id == id);
+            if(index.HasValue && product.Images.Where(i => i.Index == index).Count() == 0)
+            {
+                product.Images.FirstOrDefault(p => p.Id == imageId).Index = index.Value;
+                _productRepository.SaveChanges();
+            }
+
+            return RedirectToAction("UploadImages", new { id = id });
+        }
+
+        public ActionResult DeleteImage(string id)
+        {
+            var image = _imageRepository.FirstOrDefault(i => i.Id == id);
+            var productId = image.ProductId;
+
+            if (image != null)
+            {
+                _imageRepository.Delete(image);
+                _imageRepository.SaveChanges();
+            }
+
+            return RedirectToAction("UploadImages", new { id = productId });
+        }
+        
 
         #region Helpers
 
